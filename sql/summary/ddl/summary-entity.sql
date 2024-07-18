8d0e59c9c280ec46d03cc3f6b8a0d8a479b13d20
Douglas Brian Quayle <dbquayle@gmail.com>
2024-07-18 06:32:59 -0400

    changes

--- sql/summary/ddl/summary-entity.sql
+++ sql/summary/ddl/summary-entity.sql
@@ -12,9 +12,21 @@ create table if not exists summary.entity (
        postal_zip text not null,
        postal_country text not null,
 
-       email_address text,
-       telephone_number text not null
+       email_address text not null,
+       telephone_number text not null,
+
+       login_user text not null -- Bridge user associated with the entity
 );
 
 create unique index on summary.entity(bridge_id);
+
+/**
+	For the purposes of this exercise, we will assume that the ACL
+	is very simple and that entities can only see transfers to which they
+	are a party.  This necessitates the login_user field in the database.
+
+	Also, we will assume no cloning of entities, hence the unique index.
+*/
+create unique index on summary.entity(login_user);
+
 
